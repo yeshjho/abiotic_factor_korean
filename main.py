a0e44b694b859f37c482b551d4b5e1554b9cf1ef
yeshjho <yeshjho@naver.com>
2025-02-28 07:06:29 -0800

    prepare for 0.10.0

--- main.py
+++ main.py
@@ -35,8 +35,8 @@ data/image/[버전]/이미지+까지+경로
 """
 
 
-GAME_VERSION = "0.9.3.13049"
-PATCH_VERSION = "1.2.1"
+GAME_VERSION = "0.10.0.15580"
+PATCH_VERSION = "1.3.0"
 
 
 def visualize_whitespace(text: str):
@@ -244,7 +244,7 @@ def overwrite_fstring(data, offset, new_text, does_offset_points_size):
 
 def datatable_override(file, data, pairs):
     size_offset_map = {
-        'AbioticFactor/Content/Blueprints/DataTables/DT_Compendium.uasset': 0x2e60
+        'AbioticFactor/Content/Blueprints/DataTables/DT_Compendium.uasset': 0x3940
     }
 
     byte_difference = 0
