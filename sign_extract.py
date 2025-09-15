e87cd7c84ec5c57264d4096855ec7d7a69ca0c10
yeshjho <yeshjho2@gmail.com>
2025-09-15 17:43:33 +0800

    Improve sign extractor - there are signs other than Sign_ModularFacility

--- sign_extract.py
+++ sign_extract.py
@@ -6,16 +6,35 @@ import re
 from common import *
 
 
-is_Sign_ModularFacility_only = True
+print_all_text_render_component = False
+print_outer = True
+
+
+def is_valid_outer(outer: str):
+    if outer.startswith('Sign_ModularFacility') or outer.startswith('Sign_ModularDormitory'):
+        return True
+    if outer.startswith('TextRenderActor'):
+        return True
+
+    if outer.startswith('Trigger') or outer.startswith('Abiotic_TriggerVolume') or outer.startswith('TutorialTrigger'):
+        return False
+    if outer.startswith('CompendiumUnlock'):
+        return False
+    if outer.startswith('SimpleHatch_BP') or outer.startswith('Sliding'):
+        return False
+    if outer.startswith('StaticMeshActor'):
+        return False
+
+    raise RuntimeError(f'Add {outer} to is_valid_outer!')
 
 
 files = glob.glob(f'archive/offset_annotated/AbioticFactor/Content/Maps/**/*.*', recursive=True)
 
-csv_file = open(f'out/signs_{VERSION}{"" if is_Sign_ModularFacility_only else "_all"}.csv', 'w', newline='')
+csv_file = open(f'out/signs_{VERSION}{"_all" if print_all_text_render_component else ""}.csv', 'w', newline='')
 writer = csv.writer(csv_file, delimiter='\t')
 
-csv_file2 = open(f'out/signs_{VERSION}{"" if is_Sign_ModularFacility_only else "_all"}_no_offset.csv', 'w', newline='')
-writer2 = csv.writer(csv_file2, delimiter='\t')
+csv_file2 = open(f'out/signs_{VERSION}{"_all" if print_all_text_render_component else ""}_no_offset.csv', 'w', newline='')
+writer_no_offset = csv.writer(csv_file2, delimiter='\t')
 
 
 outer_trim_pattern = re.compile(r'_\d+$')
@@ -37,11 +56,13 @@ for file in files:
         #     continue  # 아래 것만 바꾸면 됨
         # elif object_type == 'TextRenderComponent':
         if object_type == 'TextRenderComponent':
-            outer = data['Outer']
-            if (is_Sign_ModularFacility_only and not outer.startswith('Sign_ModularFacility')) or \
-                    'Text' not in properties or \
-                    'CultureInvariantString' not in properties['Text']:
+            if 'Text' not in properties or 'CultureInvariantString' not in properties['Text']:
                 continue
+
+            outer = data['Outer']
+            if not print_all_text_render_component and not is_valid_outer(outer):
+                continue
+
             text = properties['Text']['CultureInvariantString']
         else:
             continue
@@ -56,16 +77,15 @@ for file in files:
             continue
         text = visualize_whitespace(text)
 
-        writer.writerow([
+        row = [
             f'AbioticFactor/Content/Maps/{map_name}.umap',
-            # outer,
             int(text_start) + 4,  # 텍스트 사이즈부터 시작하는데 메인 스크립트가 실제 텍스트 오프셋을 기대해서 4만큼 더하기
-            '',
+            outer,
             text
-        ])
-        writer2.writerow([
-            f'AbioticFactor/Content/Maps/{map_name}.umap',
-            # outer,
-            '',
-            text
-        ])
+        ]
+        if not print_outer:
+            del row[2]
+
+        writer.writerow(row)
+        del row[1]  # remove offset
+        writer_no_offset.writerow(row)
