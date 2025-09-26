a573f31a07664361bcdf8cab7ce7a612b289f790
yeshjho <yeshjho2@gmail.com>
2025-09-26 20:32:45 +0800

    update dialogue extract script

--- dialogue_extract.py
+++ dialogue_extract.py
@@ -4,30 +4,33 @@ import csv
 from common import *
 
 
-files = glob.glob(f'archive/pack/vanilla_extracted_parsed/{VERSION}/AbioticFactor/Content/Audio/**/*.*', recursive=True)
+files = glob.glob(f'archive/offset_annotated/AbioticFactor/Content/Audio/**/*.*', recursive=True)
 
 csv_file = open('out/dialogue_extra.csv', 'w', newline='')
 writer = csv.writer(csv_file)
 
 for file in files:
     with open(file) as f:
-        data = json.load(f)
+        data_list = json.load(f)
 
-    data = data[0]
-    if data.get('Type', '') != 'DialogueWave':
-        continue
+    for data in data_list:
+        if data.get('Type', '') != 'DialogueWave':
+            continue
 
-    properties = data['Properties']
-    if 'SpokenText' not in properties:
-        continue
+        properties = data['Properties']
+        if 'SpokenText' not in properties:
+            continue
 
-    context_mappings = properties['ContextMappings']
-    assert len(context_mappings) == 1
-    context_mapping = context_mappings[0]
-    assert context_mapping['LocalizationKeyFormat'] == '{ContextHash}'
+        context_mappings = properties['ContextMappings']
+        assert len(context_mappings) == 1
+        context_mapping = context_mappings[0]
 
-    writer.writerow([
-        properties['LocalizationGUID'].replace('-', ''),
-        context_mapping['SoundWave']['ObjectPath'],
-        data['Name'],
-    ])
+        sound_wave = context_mapping['SoundWave']
+
+        assert context_mapping['LocalizationKeyFormat'].startswith('{ContextHash}')
+
+        writer.writerow([
+            properties['LocalizationGUID'].replace('-', ''),
+            sound_wave['ObjectPath'] if sound_wave else '',
+            data['Name'],
+        ])
